035339eff78c33be5c99f4cd8eaefd97341ebe84
Nelson Stevens <nelson@cynkra.com>
2025-03-26 11:09:47 +0100

    add example

---
+++ inst/examples/add_panel/app.R
@@ -0,0 +1,120 @@
+library(shiny)
+library(bslib)
+library(visNetwork)
+
+nodes <- data.frame(id = 1:3)
+edges <- data.frame(from = c(1, 2), to = c(1, 3))
+
+ui <- page_fillable(
+  actionButton("btn", "add Panel"),
+  dock_viewOutput("dock")
+)
+
+server <- function(input, output, session) {
+  output$dock <- renderDock_view({
+    dock_view(
+      panels = list(
+        panel(
+          id = "1",
+          title = "Panel 1",
+          content = tagList(
+            sliderInput(
+              "obs",
+              "Number of observations:",
+              min = 0,
+              max = 1000,
+              value = 500
+            ),
+            plotOutput("distPlot")
+          )
+        ),
+        panel(
+          id = "2",
+          title = "Panel 2",
+          content = tagList(
+            visNetworkOutput("network")
+          ),
+          position = list(
+            referencePanel = "1",
+            direction = "right"
+          ),
+          minimumWidth = 500
+        ),
+        panel(
+          id = "3",
+          title = "Panel 3",
+          content = tagList(
+            selectInput(
+              "variable",
+              "Variable:",
+              c("Cylinders" = "cyl", "Transmission" = "am", "Gears" = "gear")
+            ),
+            tableOutput("data")
+          ),
+          position = list(
+            referencePanel = "2",
+            direction = "below"
+          )
+        )
+      ),
+      theme = "replit"
+    )
+  })
+
+  output$distPlot <- renderPlot({
+    req(input$obs)
+    hist(rnorm(input$obs))
+  })
+
+  output$network <- renderVisNetwork({
+    visNetwork(nodes, edges, width = "100%")
+  })
+
+  output$data <- renderTable(
+    {
+      mtcars[, c("mpg", input$variable), drop = FALSE]
+    },
+    rownames = TRUE
+  )
+
+  output$plot <- renderPlot({
+    dist <- switch(input$dist,
+      norm = rnorm,
+      unif = runif,
+      lnorm = rlnorm,
+      exp = rexp,
+      rnorm
+    )
+
+    hist(dist(500))
+  })
+
+  observeEvent(input$btn, {
+    pnl <- panel(
+      id = "new_1",
+      title = "Dynamic panel",
+      content = tagList(
+        radioButtons(
+          "dist", "Distribution type:",
+          c(
+            "Normal" = "norm",
+            "Uniform" = "unif",
+            "Log-normal" = "lnorm",
+            "Exponential" = "exp"
+          )
+        ),
+        plotOutput("plot")
+      ),
+      position = list(
+        referencePanel = "1",
+        direction = "within"
+      )
+    )
+    add_panel(
+      "dock",
+      pnl
+    )
+  })
+}
+
+shinyApp(ui, server)
