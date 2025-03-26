c641d3d5afdd066dbfc3e2097572763e8e085073
Nelson Stevens <nelson@cynkra.com>
2025-03-26 11:30:57 +0100

    specify shiny::icon

--- R/dock_view.R
+++ R/dock_view.R
@@ -118,7 +118,7 @@ dock_view <- function(
     dependencies = c(
       unlist(deps, recursive = FALSE),
       # Add fontawesome (avoids to get dependency on {fontawesome})
-      htmltools::findDependencies(icon("cogs"))
+      htmltools::findDependencies(shiny::icon("cogs"))
     ),
     width = width,
     height = height,
