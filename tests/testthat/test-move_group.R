3dc1f344b55bd4a359fee0ae6cb36a698b27fa15
Nelson Stevens <nelson@cynkra.com>
2025-04-18 23:01:24 +0200

    adapt tests

--- tests/testthat/test-move_group.R
+++ tests/testthat/test-move_group.R
@@ -1,20 +1,9 @@
 library(shinytest2)
 
-test_that("move_group works", {
-  # Don't run these tests on the CRAN build servers
-  skip_on_cran()
+test_that("move_group app works", {
   appdir <- system.file(package = "dockViewR", "examples", "move_group")
 
-  app <- AppDriver$new(
-    appdir,
-    name = "move_group",
-    seed = 121,
-    height = 752,
-    width = 1211
-  )
-  app$wait_for_idle()
+  app <- AppDriver$new(appdir, name = "move_group", seed = 121, height = 752, width = 1211)
   app$expect_values(input = TRUE, output = FALSE, export = TRUE)
   app$click("move2")
-  app$wait_for_idle()
-  app$expect_values(input = TRUE, output = FALSE, export = TRUE)
 })
